e8e401fa123de9bff0f1522247766b4f420d57ba
Marco van Poortvliet <Marc0tjevp@gmail.com>
2017-09-27 15:15:21 +0200

    Added some comments

--- Lesson 1/Lesson 1 - Homework.sql
+++ Lesson 1/Lesson 1 - Homework.sql
@@ -1,15 +1,25 @@
+/* Opdracht 1 */
+
 SELECT Titel
 FROM Boek;
+
+/* Opdracht 2 */
 
 SELECT Auteur, Titel
 FROM Boek;
 
+/* Opdracht 3 */
+
 SELECT DISTINCT Auteur
 FROM Boek;
+
+/* Opdracht 4 */
 
 SELECT Titel
 FROM Boek
 WHERE ISBN < 4000;
+
+/* Opdracht 5 */
 
 SELECT Titel
 FROM Boek
@@ -18,13 +28,19 @@ WHERE
 	OR ISBN > 7000
 ORDER BY Titel;
 
+/* Opdracht 6 */
+
 SELECT Titel
 FROM Boek
 WHERE Auteur = 'Jerzy Kosinski';
 
+/* Opdracht 7 */
+
 SELECT Titel
 FROM Boek
 WHERE Auteur LIKE '%Reve%';
+
+/* Opdracht 8 */
 
 SELECT Titel, Auteur, ISBN
 FROM boek
--- Lesson 2/Lesson 2 - Homework.sql
+++ Lesson 2/Lesson 2 - Homework.sql
@@ -1,31 +1,47 @@
+/* Opdracht 1 */
+
 SELECT Auteur, COUNT(Titel) AS CountTitle
 FROM Boek
 Group By Auteur;
 
+/* Opdracht 2 */
+
 SELECT COUNT(ISBN) AS CountISBN
 FROM Boek
 WHERE ISBN > 1012;
+
+/* Opdracht 3 */
 
 SELECT Auteur, COUNT(Titel) AS CountTitle
 FROM Boek
 GROUP BY Auteur
 HAVING COUNT(Titel) >= 2;
 
+/* Opdracht 4 */
+
 SELECT Auteur, MAX(editie) AS MaxEdition, MIN(editie) AS MinEdition
 FROM Boek
 GROUP BY Auteur;
+
+/* Opdracht 5 */
 
 SELECT Titel, AVG(editie) AS AverageEditions
 FROM Boek
 WHERE Titel LIKE 'D%'
 GROUP BY Titel;
 
+/* Opdracht 6 */
+
 SELECT SUM(editie) AS SumEditions
 FROM Boek
 WHERE Auteur IN('Harry Mulisch', 'Jan Cremer');
 
+/* Opdracht 7 */
+
 SELECT YEAR(DatumTerugave) AS Year, MONTH(DatumTerugave) AS Month, DAY(DatumTerugave) AS Day
 FROM Uitlening;
+
+/* Opdracht 8 */
 
 SELECT CONVERT(date, DatumTerugave) AS DateReturn, CONVERT(date, CURRENT_TIMESTAMP) AS DateToday, DATEDIFF(dd, DatumTerugave, GETDATE()) AS DaysPassed
 FROM Uitlening;
